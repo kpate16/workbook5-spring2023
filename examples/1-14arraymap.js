81f49b970eaf2637d122ec0164e5cf50e168aa18
Kanchan Pate <kpate@pgh.yearup.org>
2023-05-16 14:41:21 -0400

    examples and exercise

---
+++ examples/1-14arraymap.js
@@ -0,0 +1,17 @@
+let kids = [
+    { first: "Natalie", last: "Plyers" },
+    { first: "Brittany", last: "Ray" },
+    { first: "Zachary", last: "Westly" },
+  ];
+  
+  function buildFullName(kid) {
+    return kid.last + ", " + kid.first;
+  }
+  
+  function displayName(kid) {
+    console.log(kid);
+  }
+  
+  let namesList = kids.map(buildFullName);
+  
+  namesList.forEach(displayName);
